245449715f0fbb7a6b26b617512ff75c9341ce5b
Leonardo da Silva Fanck <leonardofanck@gmail.com>
2026-06-30 19:59:54 -0300

    Adicionado nova tabela teste

---
+++ src/RegistroReceitas/RegistroReceitas/Migrations/20260630225832_Adicionado classe teste.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroReceitas.Migrations
+{
+    /// <inheritdoc />
+    public partial class Adicionadoclasseteste : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ src/RegistroReceitas/RegistroReceitas/Models/Teste.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RegistroReceitas.Models;
+
+public class Teste
+{
+    public Guid Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+}
